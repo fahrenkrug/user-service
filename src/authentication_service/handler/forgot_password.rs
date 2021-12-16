e3bbf9df6758b240b67301b75d24680017ca96a9
finnfahrenkrug <finnfahrenkrug@gmail.com>
2021-12-16 16:42:12 +0100

    Add todo list.

--- src/authentication_service/handler/forgot_password.rs
+++ src/authentication_service/handler/forgot_password.rs
@@ -3,5 +3,6 @@ use super::super::{ForgotPasswordRequest, ForgotPasswordResponse};
 
 pub fn respond(request: Request<ForgotPasswordRequest>) -> Result<Response<ForgotPasswordResponse>, Status> {
     println!("Got forgot_password request: {:?}", request);
+    todo!("Come back here once we have a notification service ready");
     Ok(Response::new(ForgotPasswordResponse {}))
 }
